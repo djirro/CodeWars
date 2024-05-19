8c5a32b2b884cff9bfed639bb15e3e2c26e84963
Егор Даншов <>
2024-05-19 21:27:38 +0300

    Решил задание 8kyo OppositesAttract

--- src/EightKyo/OppositesAttract.java
+++ src/EightKyo/OppositesAttract.java
@@ -2,7 +2,10 @@ package EightKyo;
 
 public class OppositesAttract {
     public static boolean isLove(final int flower1, final int flower2) {
-        return true;
+        var sumPetals = flower1 + flower2;
+        var evenOdd = sumPetals % 2;
+
+        return evenOdd == 1;
     }
 }
 
@@ -16,4 +19,27 @@ and the other has an odd number of petals it means they are in love.
 
 Write a function that will take the number of petals of each flower
 and return true if they are in love and false if they aren't.
+ */
+
+
+/*
+Лучшее решение:
+
+public class OppositesAttract {
+
+  public static boolean isLove(final int flower1, final int flower2) {
+    return flower1 % 2 != flower2 % 2;
+  }
+
+}
+
+Или такое:
+
+public class OppositesAttract {
+
+  public static boolean isLove(final int flower1, final int flower2) {
+  return (flower1+flower2)%2!=0;
+  }
+
+}
  */
