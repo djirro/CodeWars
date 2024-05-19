5e40472243ca1f469e4ba296de816edce0fbcc38
Егор Даншов <>
2024-05-19 16:14:27 +0300

    Создал в классе Main небольшое рудоководство для работы

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello, CodeWars!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        /*
+        Я буду разбираться по ходу.
+        Для начала я создам папки со сложностями задачь.
+        Под каждую задачу буду выводить новую ветку.
+        После каждой успешно сделанной задачи - буду мержить ветку в dev.
+        Рабочая ветка - dev.
+         */
     }
 }
