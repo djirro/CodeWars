b8100ca1be8d8fb1e12a068a278e130c8497660a
Егор Даншов <>
2024-05-20 15:29:43 +0300

    внес правки в коммент

--- src/EightKyo/OppositesAttract.java
+++ src/EightKyo/OppositesAttract.java
@@ -38,8 +38,10 @@ public class OppositesAttract {
 public class OppositesAttract {
 
   public static boolean isLove(final int flower1, final int flower2) {
-  return (flower1+flower2)%2!=0;
+    return (flower1+flower2) %2 !=0;
   }
 
 }
+
+если честно, не выглядят они достаточно понятными в коде. Менее читабельны чтоли.
  */
