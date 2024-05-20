e873bb935f23e7b3df9426342d2f14fd185ccff4
Егор Даншов <>
2024-05-20 16:09:25 +0300

    Создал ветку для задания. Добавил комментарии

---
+++ src/EightKyo/RockPaperScissors.java
@@ -0,0 +1,28 @@
+package EightKyo;
+
+public class RockPaperScissors {
+    public static String rps(String p1, String p2) {
+
+        return "";
+    }
+}
+
+/*
+Задание:
+Игра камень ножницы бумага.
+На вход подается два String. Первый и второй
+Указывают они одно из этого:
+rock - камень
+paper - бумага
+scissors - ножницы
+
+Метод должен вернуть:
+Player 1 won! - победа первого игрока
+Player 2 won! - победа второго игрока
+Draw! - ничья
+ */
+
+/*
+Ссылка:
+https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/java
+*/
