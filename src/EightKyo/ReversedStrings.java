eb55dbf96c72466b1b7855fac3b59620a5ddc5c4
Егор Даншов <>
2024-05-24 18:05:19 +0300

    Создал ветку 8kyo-ReversedStrings. Добавил комментарии.

---
+++ src/EightKyo/ReversedStrings.java
@@ -0,0 +1,23 @@
+package EightKyo;
+
+public class ReversedStrings {
+    public static String solution(String str) {
+
+        return "";
+    }
+}
+
+
+/*
+Задание:
+Написать метод, который принимает на вход строку
+и переворачивает ее.
+
+Пример:
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+ */
+
+/*
+Ссылка: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/java
+ */
