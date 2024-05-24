b08b52de918cbfb39a81dfb5ab6ac0ef9a772e41
Егор Даншов <>
2024-05-24 18:27:29 +0300

    Решил задачу.

--- src/EightKyo/ReversedStrings.java
+++ src/EightKyo/ReversedStrings.java
@@ -2,11 +2,14 @@ package EightKyo;
 
 public class ReversedStrings {
     public static String solution(String str) {
-
-        return "";
+        return new StringBuilder(str).reverse().toString();
     }
 }
 
+/*
+Кто-то сказал что лучше использовать StringBuffer() вместо StringBuilder(),
+так как StringBuffer() - потокобезопасен.
+ */
 
 /*
 Задание:
@@ -16,8 +19,6 @@ public class ReversedStrings {
 Пример:
 'world'  =>  'dlrow'
 'word'   =>  'drow'
- */
 
-/*
 Ссылка: https://www.codewars.com/kata/53369039d7ab3ac506000467/train/java
  */
